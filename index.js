837aeb577e4308cdbe1cd4d91c04ce5adcb0c8bf
UttamSaha <uttamsaha982@gmail.com>
2022-04-24 20:43:27 +0600

    get method added

--- index.js
+++ index.js
@@ -10,6 +10,33 @@ app.use(express.json())
 require('dotenv').config();
 
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.86nll.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run(){
+    try{
+        await client.connect();
+        const productCollection = client.db('emaJhon').collection('product');
+
+
+        //Read all data
+       app.get('/product', async(req,res)=>{
+        const query = {};
+        const cursor = productCollection.find(query);
+        const products = await cursor.toArray();
+        res.send(products);
+       })
+
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir);
+
 app.get('/',(req,res)=>{
     res.send('Ema - Jhon server running..');
 })
